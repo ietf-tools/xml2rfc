b03f420738c992fffae16c7dc48db13337a1ec32
Wiggins <wiggins@concentricsky.com>
2011-05-04 20:10:41 +0000

    RawTxt writer outputs header  - Legacy-Id: 172

--- version2/xml2rfc/writer.py
+++ version2/xml2rfc/writer.py
@@ -1,12 +1,11 @@
 """ Writer classes to output rfc data to various formats """
 
 def justify_inline(left_str, center_str, right_str, width=80):
-    """ Takes three string arguments and outputs a single string with
-        the arguments left-justified, centered, and right-justified
-        respectively.
+    """ Takes three string arguments and outputs a single string with the 
+        arguments left-justified, centered, and right-justified respectively.
         
-        Throws an exception if the combined length of the three strings
-        is greater than the width.
+        Throws an exception if the combined length of the three strings is 
+        greater than the width.
     """
 
     if (len(left_str) + len(center_str) + len(right_str)) > width:
@@ -21,13 +20,54 @@ def justify_inline(left_str, center_str, right_str, width=80):
 
 class XmlRfcWriter:
     """ Base class for all writers """
-    rfctree = None
-    
-    def __init__(self, rfctree):
-        self.rfctree = rfctree
+    rfc = None
+
+    def __init__(self, rfc):
+        self.rfc = rfc
         
     def write(self, filename):
         raise NotImplementedError('write() must be overridden')
 
+
 class RawTextRfcWriter(XmlRfcWriter):
-    pass
+    """ Writes to a text file, unpaginated, no headers or footers. """
+    
+    def write(self, filename):
+        out = []
+        
+        # Front page, left heading
+        fp_left = [self.rfc.attribs['trad_header']]
+        if 'number' in self.rfc.attribs:
+            fp_left.append(self.rfc.attribs['number'])
+        if 'updates' in self.rfc.attribs:
+            fp_left.append(self.rfc.attribs['updates'])
+        if 'obsoletes' in self.rfc.attribs:
+            fp_left.append(self.rfc.attribs['obsoletes'])
+        if 'category' in self.rfc.attribs:
+            fp_left.append(self.rfc.attribs['category'])
+        
+        # Front page, right heading
+        fp_right = []
+        for author in self.rfc['front']['author']:
+            fp_right.append(author.attribs['initials'] + ' ' + \
+                            author.attribs['surname'])
+            if 'organization' in author:
+                fp_right.append(author['organization'].text)
+        date = self.rfc['front']['date']
+        fp_right.append(date.attribs['month'] + ' ' + date.attribs['year'])
+        
+        # Construct full heading
+        for i in range(max(len(fp_left), len(fp_right))):
+            if i < len(fp_left): left = fp_left[i]; 
+            else: left = '';
+            if i < len(fp_right): right = fp_right[i]; 
+            else: right = '';
+            out.append(justify_inline(left, '', right))
+        out.append('\n' + self.rfc['front']['title'].text.center(80) + '\n')
+            
+        # Write everything to file
+        file = open(filename, 'w')
+        for line in out:
+            file.write(line)
+            file.write('\n')
+ 
