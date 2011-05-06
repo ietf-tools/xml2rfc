4ea66ad54956bdcf5904225f6be417fc52770eae
Wiggins <wiggins@concentricsky.com>
2011-05-06 22:41:07 +0000

    PEP-8 Fixes  - Legacy-Id: 221

--- version2/test.py
+++ version2/test.py
@@ -6,9 +6,16 @@ import sys
 
 def main():
     if len(sys.argv) > 1:
+<<<<<<< HEAD
         parser = xml2rfc.parser.XmlRfcParser()
         xmlrfc = parser.parse(str(sys.argv[1]))
         writer = xml2rfc.writers.RawTextRfcWriter(xmlrfc)
+=======
+        tree = xml2rfc.parser.XmlRfc()
+        parser = xml2rfc.parser.XmlRfcParser(tree)
+        writer = xml2rfc.writers.RawTextRfcWriter(tree)
+        parser.parse(str(sys.argv[1]))
+>>>>>>> PEP-8 Fixes
         writer.write('output.txt')
     else:
         print ("Needs xml file argument")
