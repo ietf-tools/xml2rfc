2ffd4e2059227ea92ac54d74ec77555cb9fc0cf3
Josh Bothun <jbothun@concentricsky.com>
2011-05-12 22:03:17 +0000

    added a default header to nroff writer that gets inserted during pre_processing()  - Legacy-Id: 289

--- version2/test.py
+++ version2/test.py
@@ -8,8 +8,8 @@ def main():
     if len(sys.argv) > 1:
         parser = xml2rfc.parser.XmlRfcParser()
         xmlrfc = parser.parse(str(sys.argv[1]))
-        writer = xml2rfc.writers.HtmlRfcWriter(xmlrfc)
-        writer.write('output.html')
+        writer = xml2rfc.writers.NroffRfcWriter(xmlrfc)
+        writer.write('output.nroff')
     else:
         print ("Needs xml file argument")
 
--- version2/xml2rfc/writers/nroff.py
+++ version2/xml2rfc/writers/nroff.py
@@ -1,7 +1,24 @@
 # Local libs
 from raw_txt import RawTextRfcWriter
 
+default_header = ['.pl 10.0i',      # Page length
+                  '.po 0',          # Page offset
+                  '.ll 7.2i',       # Line length
+                  '.lt 7.2i',       # Title length
+                  '.nr LL 7.2i',    # Printer line length
+                  '.nr LT 7.2i',    # Printer title length
+                  ]
 
 class NroffRfcWriter(RawTextRfcWriter):
     """ Writes to an nroff file """
-    pass
+    
+    def __init__(self, xmlrfc):
+        RawTextRfcWriter.__init__(self, xmlrfc)
+        
+    # ---------------------------------------------------------
+    # RawTextRfcWriter overrides
+    # ---------------------------------------------------------
+
+    def pre_processing(self):
+        """ Inserts the standard nroff header to the buffer """
+        self.buf.extend(default_header)
