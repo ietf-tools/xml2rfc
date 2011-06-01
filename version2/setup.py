21ae4af43ac84c1d4fe4ffe52a101f62f267ebdb
Josh Bothun <jbothun@concentricsky.com>
2011-06-01 17:25:53 +0000

    changed email in setup.py  - Legacy-Id: 311

--- version2/setup.py
+++ version2/setup.py
@@ -7,12 +7,11 @@ setup(name='xml2rfc',
     description='Validate and convert XML RFC documents to various output ' \
                   'formats',
     author='Concentric Sky',
-    author_email='',
+    author_email='tools-discuss@ietf.org',
     url='',
     scripts=['scripts/xml2rfc'],
     packages=['xml2rfc', 'xml2rfc/writers'],
-    package_data={'xml2rfc': [
-                                'templates/*'
-                                ]},
+    package_data={'xml2rfc': ['templates/*'
+                              ]},
     install_requires=['lxml>=2.3'],
 )
