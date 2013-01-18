d7e0e1a92ac30fd65569d8e48b547af26bbc4187
Henrik Levkowetz <henrik@levkowetz.com>
2013-01-18 17:05:54 +0000

    Added the 'date' keyword argument to the ExpandedXmlWriter's __init__(), too.  - Legacy-Id: 695

--- version2/cli/xml2rfc/writers/expanded_xml.py
+++ version2/cli/xml2rfc/writers/expanded_xml.py
@@ -4,7 +4,7 @@
 
 import lxml.etree
 import xml2rfc
-
+import datetime
 
 class ExpandedXmlWriter:
     """ Writes a duplicate XML file but with all references expanded """
@@ -12,7 +12,7 @@ class ExpandedXmlWriter:
     # Note -- we don't need to subclass BaseRfcWriter because the behavior
     # is so different and so trivial
 
-    def __init__(self, xmlrfc, quiet=False, verbose=False):
+    def __init__(self, xmlrfc, quiet=False, verbose=False, date=datetime.date.today()):
         self.root = xmlrfc.getroot()
         self.quiet = quiet
         self.verbose = verbose
