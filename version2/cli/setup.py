24881daf2ae663db44de079b655a8ac103ac2526
Henrik Levkowetz <henrik@levkowetz.com>
2013-01-20 17:51:56 +0000

    Bumped the version in setup.py  - Legacy-Id: 708

--- version2/cli/setup.py
+++ version2/cli/setup.py
@@ -54,7 +54,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.3.11',
+    version='2.3.11.1',
     author='Josh Bothun (Concentric Sky)',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
