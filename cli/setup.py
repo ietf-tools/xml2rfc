a88d0cc30546196373c215f1dc31e25f528ba72f
Henrik Levkowetz <henrik@levkowetz.com>
2013-01-31 23:07:19 +0000

    Bumped the version number to 2.4.0.  - Legacy-Id: 807

--- cli/setup.py
+++ cli/setup.py
@@ -54,7 +54,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.3.12.dev',
+    version='2.4.0',
     author='Josh Bothun (Concentric Sky)',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
