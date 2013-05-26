c0c6a5344fc6bfbb383715f2a001ee4b6206c0a3
Henrik Levkowetz <henrik@levkowetz.com>
2013-05-26 21:43:22 +0000

    Make VERSION work with non-numeric subversions, too.  - Legacy-Id: 891

--- cli/xml2rfc/__init__.py
+++ cli/xml2rfc/__init__.py
@@ -1,9 +1,9 @@
 
 
 # Static values
-__version__  = '2.4.2'
+__version__  = '2.4.2.rc1'
 NAME         = 'xml2rfc'
-VERSION      = [ int(i) for i in __version__.split('.') ]
+VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
 CACHES       = ['/var/cache/xml2rfc', '~/.cache/xml2rfc']  # Ordered by priority
 CACHE_PREFIX = ''
 NET_SUBDIRS  = ['bibxml', 'bibxml2', 'bibxml3', 'bibxml4', 'bibxml5']
