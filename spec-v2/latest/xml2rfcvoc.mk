76e355423fdf59285b463b5803728489e3ff290c
Julian Reschke <julian.reschke@gmx.de>
2013-11-21 20:43:26 +0000

    backup source before overwriting it  - Legacy-Id: 969

--- spec-v2/latest/xml2rfcvoc.mk
+++ spec-v2/latest/xml2rfcvoc.mk
@@ -20,6 +20,7 @@ xml2rfcv2.rnc.folded: xml2rfcv2.rnc
 	fold -w69 -s $< > $@
 
 draft-reschke-xml2rfc-latest.xml: xml2rfcv2-spec.xml xml2rfcv2.rnc.folded
+	cp -v $@ $@.bak
 	./refresh-inclusions.sh $@
 
 trang.jar:	trang-20091111.zip
