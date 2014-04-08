79240506c3d3903797f7f577402ad7ecd5a24804
Julian Reschke <julian.reschke@gmx.de>
2014-04-08 10:50:41 +0000

    -07  - Legacy-Id: 1337

---
+++ vocabulary/v2/07/xml2rfcvoc.mk
@@ -0,0 +1,31 @@
+xml2rfc.all: \
+	draft-reschke-xml2rfc-latest.xml xml2rfcv2-annotated.rng
+
+xml2rfcv1.rnc: xml2rfcv1.dtd trang.jar
+	java -jar trang.jar -i inline-attlist $< $@
+
+xml2rfcv2.rnc: xml2rfcv2.dtd trang.jar
+	java -jar trang.jar -i inline-attlist $< $@
+
+xml2rfcv2.rng: xml2rfcv2.dtd trang.jar
+	java -jar trang.jar -i inline-attlist $< $@
+
+xml2rfcv2-annotated.rng: xml2rfcv2.rng annotate-rng.xslt draft-reschke-xml2rfc-latest.xml
+	saxon $< annotate-rng.xslt doc=draft-reschke-xml2rfc-latest.xml > $@
+
+xml2rfcv2-spec.xml: xml2rfcv2.rng rng2xml2rfc.xslt
+	saxon $< rng2xml2rfc.xslt > $@
+
+xml2rfcv2.rnc.folded: xml2rfcv2.rnc
+	fold -w69 -s $< | sed "s/\&/\&amp;/g" | sed "s/</\&lt;/g" | sed "s/>/\&gt;/g" > $@
+
+draft-reschke-xml2rfc-latest.xml: xml2rfcv2-spec.xml xml2rfcv2.rnc.folded
+	cp -v $@ $@.bak
+	./refresh-inclusions.sh $@
+
+trang.jar:	trang-20091111.zip
+	unzip -j trang-20091111.zip trang-20091111/trang.jar
+
+trang-20091111.zip:
+	wget http://jing-trang.googlecode.com/files/trang-20091111.zip || \
+	curl -o trang-20091111.zip http://jing-trang.googlecode.com/files/trang-20091111.zip
