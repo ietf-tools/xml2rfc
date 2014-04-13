96e26d4a02ee7e08468d99c639440118c9c82f5a
Julian Reschke <julian.reschke@gmx.de>
2014-04-13 17:52:38 +0000

    add make target for RNC differences file (needs more work for formatting)  - Legacy-Id: 1350

--- vocabulary/v3/latest/xml2rfcvoc.mk
+++ vocabulary/v3/latest/xml2rfcvoc.mk
@@ -27,3 +27,15 @@ trang.jar:	trang-20091111.zip
 trang-20091111.zip:
 	wget http://jing-trang.googlecode.com/files/trang-20091111.zip || \
 	curl -o trang-20091111.zip http://jing-trang.googlecode.com/files/trang-20091111.zip
+
+
+xml2rfcv2 = ../../v2/06/xml2rfcv2.rnc
+
+differences-from-v2.txt:	xml2rfcv3.rnc $(xml2rfcv2)
+	fold -w66 -s $(xml2rfcv2) > $@.v2
+	fold -w66 -s $<  > $@.v3
+	diff -w -c20 $@.v2 $@.v3 | sed "s/\&/\&amp;/g" > $@
+	rm -f $@.v2 $@.v3
+
+
+	
