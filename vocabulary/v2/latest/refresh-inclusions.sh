422e7678d977aeac5d4c7f4c89e8ce241d0f1472
Julian Reschke <julian.reschke@gmx.de>
2014-04-29 13:44:26 +0000

    switch to new version of refresh-inclusions  - Legacy-Id: 1388

--- vocabulary/v2/latest/refresh-inclusions.sh
+++ vocabulary/v2/latest/refresh-inclusions.sh
@@ -4,33 +4,76 @@
 expand() {
 
   cat $1 | awk ' 
+
+  function filecontents(filename) {
+    while (getline < filename > 0) {
+      fc[filename] = fc[filename] $0 "\n"
+    }
+    return fc[filename]
+  }
+
   BEGIN {
     includefile = "";
+    includeescapedfile = "";
   }
   
-  # start include
-  /^<\?BEGININC .* \?>$/ {
+  # start include (verbatim mode)
+  /<\?BEGININC .* \?>$/ {
     print
-    includefile = $2
-    while (getline <includefile > 0) {
-      print
-    }
+    keyword = "<?BEGININC " 
+    extract = match($0, /<\?BEGININC .* \?>$/)
+    includefile = substr($0, RSTART + length(keyword), RLENGTH - 3 - length(keyword))
+    output = filecontents(includefile)
+    printf("%s", output)
   }
   
-  # end include
-  /^<\?ENDINC .* \?>$/ {
+  # start include (escape-for-XML mode)
+  /<\?BEGINESCAPEDINC .* \?>$/ {
+    print
+    keyword = "<?BEGINESCAPEDINC " 
+    extract = match($0, /<\?BEGINESCAPEDINC .* \?>$/)
+    includeescapedfile = substr($0, RSTART + length(keyword), RLENGTH - 3 - length(keyword))
+    output = filecontents(includeescapedfile)
+    # escape ampersand, less-than, and greater-than if part of a CDATA end marker
+    gsub(/&/, "\\&amp;", output)
+    gsub(/</, "\\&lt;", output)
+    gsub(/]]>/, "]]\\&gt;", output)
+    printf("%s", output)
+  }
+
+  # end include (verbatim mode)
+  /^<\?ENDINC .* \?>/ {
     if ($2 != includefile) {
-      printf ("unexpected ENDINC, got %s but expected %s\n", $2, includefile) >> /dev/stderr
+      printf ("unexpected ENDINC, got %s but expected %s\n", $2, includefile) >> "/dev/stderr"
     }
     includefile = "";
   }
   
+  # end include (escape-for-XML mode)
+  /^<\?ENDESCAPEDINC .* \?>/ {
+    if ($2 != includeescapedfile) {
+      printf ("unexpected ENDESCAPEDINC, got %s but expected %s\n", $2, includeescapedfile) >> "/dev/stderr"
+    }
+    includeescapedfile = "";
+  }
+
   #default
   {
-    if (includefile == "") {
+    if (includefile == "" && includeescapedfile == "") {
       print
     }
-  }' > $$
+  }
+  
+  END {
+    if (includefile != "") {
+      printf ("missing ENDINC for %s\n", includefile) >> "/dev/stderr"
+    }
+    if (includeescapedfile != "") {
+      printf ("missing ENDESCAPEDINC for %s\n", includeescapedfile) >> "/dev/stderr"
+    }
+  }
+  
+  ' > $$
   
   # check for changes
   
@@ -45,4 +88,3 @@ for i in $*
 do
   expand $i
 done
-
