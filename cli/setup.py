81adf0cc460c3e2993fd674edb94f2ce3d1bbfdf
Henrik Levkowetz <henrik@levkowetz.com>
2014-05-15 17:08:59 +0000

    Use a development version number.  - Legacy-Id: 1428

--- cli/setup.py
+++ cli/setup.py
@@ -62,7 +62,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.4.5',
+    version='2.4.6-dev',
     author='Josh Bothun (Concentric Sky)',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
