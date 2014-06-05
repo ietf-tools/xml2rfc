04fa4e673334eabbcec05c9aaf89c5a8bed479c2
Henrik Levkowetz <henrik@levkowetz.com>
2014-06-05 20:45:49 +0000

    Set version info back to development mode  - Legacy-Id: 1479

--- cli/setup.py
+++ cli/setup.py
@@ -62,7 +62,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.4.8',
+    version='2.4.9dev0',
     author='Josh Bothun (Concentric Sky)',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
