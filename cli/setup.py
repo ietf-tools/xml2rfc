591c716fae62b5638a38eeeac158c0a8c5b7c271
Henrik Levkowetz <henrik@levkowetz.com>
2014-06-05 20:42:27 +0000

    Set version info to release version 2.4.8  - Legacy-Id: 1477

--- cli/setup.py
+++ cli/setup.py
@@ -62,7 +62,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.4.8dev1',
+    version='2.4.8',
     author='Josh Bothun (Concentric Sky)',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
