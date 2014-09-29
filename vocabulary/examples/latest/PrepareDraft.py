87c7a14eec288306054da60afe677373116c87f7
Paul Hoffman <paul.hoffman@vpnc.org>
2014-09-29 01:22:52 +0000

    Got v2 working, about to try v3  - Legacy-Id: 1589

--- vocabulary/examples/latest/PrepareDraft.py
+++ vocabulary/examples/latest/PrepareDraft.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
+import subprocess
+
+DraftInputFile = "draft-hoffman-rfcexamples-input.xml"
+DraftOutputFile = "draft-hoffman-rfcexamples-latest.xml"
+ExampleFullV2File = "example-full-v2.xml"
+V2FullMarker = "<!-- INSERTV2FULL-->"
 
 # Make sure the parts are there
 try:
-	FullDraftText = open("draft-hoffman-rfcexamples-latest", mode="r")
+	FullDraftText = open(DraftInputFile, mode="r").read()
 except:
-	exit("Weird: could not find 'draft-hoffman-rfcexamples-latest'. Exiting.")
-
+	exit("Could not find '{}'. Exiting.".format(DraftInputFile))
 try:
-	FullV2ExampleText = open("example-full-v2.xml", mode="r")
+	FullV2ExampleText = open(ExampleFullV2File, mode="r").read()
 except:
-	exit("Weird: could not find 'example-full-v2.xml'. Exiting.")
+	exit("Could not find '{}'. Exiting.".format(ExampleFullV2File))
 
-FigWrapper = '''<figure>
-<artwork>
-<![CDATA[
-]]>
-</artwork>
-</figure>
-'''
+# Escaping is hard, let's go shopping
+def EscapedFigure(InText):
+	OutText = InText.replace("&", "&amp;")
+	OutText = OutText.replace("<", "&lt;")
+	FigStart = "<figure><artwork>"
+	FigEnd = "</artwork></figure>"
+	return(FigStart + OutText + FigEnd)
 
+# Replace the v2 full comment with the escaped replacement
+if not  (V2FullMarker in FullDraftText):
+	exit("Could not find '{}' in the draft input. Exiting.".format(V2FullMarker))
+FullDraftText = FullDraftText.replace(V2FullMarker, EscapedFigure(FullV2ExampleText))
+
+# Write out the coverted stuff
+OutF = open(DraftOutputFile, mode="w")
+OutF.write(FullDraftText)
+OutF.close()
+
+OutFileNameBase = DraftOutputFile[:-4]
+
+# Prepare the .txt file; this possibly only works on Paul's machine
+p = subprocess.Popen("xml2rfc {}.xml --filename={}.txt --text".format(OutFileNameBase, OutFileNameBase),
+	stdout=subprocess.PIPE, shell=True)
+p.wait()
+TheOutput = (p.stdout.read()).decode("ascii")
+print("Running xml2rfc said:\n{}".format(TheOutput))
