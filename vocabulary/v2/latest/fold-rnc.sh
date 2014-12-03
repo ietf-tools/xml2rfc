60d5658970d4ce056558d3ed1435b6a2a9d14ace
Julian Reschke <julian.reschke@gmx.de>
2014-12-03 13:08:24 +0000

    RNC markup generation: merge back change needed in v3  - Legacy-Id: 1703

--- vocabulary/v2/latest/fold-rnc.sh
+++ vocabulary/v2/latest/fold-rnc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 fold -w69 -s < $1 | \
-sed 's|^\([-a-zA-Z\]*\)* =|\
+sed 's|^\([-a-zA-Z0-9\]*\)* =|\
 <b anchor="grammar.\1">\1</b><iref item="\1 element"/> =|' | \
 sed 's|anchor="grammar\.\\|anchor="grammar\.|' | \
 sed 's|item="\\|item="|' | \
