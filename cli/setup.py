74f72e03fc9d0fcc863be0e7abdd0477d18dd325
Henrik Levkowetz <henrik@levkowetz.com>
2015-02-28 20:45:48 +0000

    Set version info to release version 2.4.9  - Legacy-Id: 1799

--- cli/setup.py
+++ cli/setup.py
@@ -62,7 +62,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.4.9.dev0',
+    version='2.4.9',
     author='Josh Bothun, Henrik Levkowetz ',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
