da1f5abb436e85700ac1a4229c0d22a77bc076fd
Julian Reschke <julian.reschke@gmx.de>
2015-04-13 19:47:48 +0000

    use strong instead of b  - Legacy-Id: 1859

--- vocabulary/v3/latest/fold-rnc.sh
+++ vocabulary/v3/latest/fold-rnc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 fold -w69 -s < $1 | \
 sed 's|^\([-a-zA-Z0-9\]*\)* =|\
-<b anchor="grammar.\1">\1</b><iref item="\1 element"/> =|' | \
+<strong anchor="grammar.\1">\1</strong><iref item="\1 element"/> =|' | \
 sed 's|anchor="grammar\.\\|anchor="grammar\.|' | \
 sed 's|item="\\|item="|' | \
-sed 's|<b anchor="grammar.start">start</b><iref item="start element"/>|start|'
+sed 's|<strong anchor="grammar.start">start</strong><iref item="start element"/>|start|'
