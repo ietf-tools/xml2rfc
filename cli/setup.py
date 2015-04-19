142b47348bf9309d917a17b4238eb6b428dd845a
Henrik Levkowetz <henrik@levkowetz.com>
2015-04-19 17:29:00 +0000

    Set version info to release version 2.4.12 before branching.  - Legacy-Id: 1887

--- cli/setup.py
+++ cli/setup.py
@@ -62,7 +62,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.4.12.dev0',
+    version='2.4.12',
     author='Josh Bothun, Henrik Levkowetz ',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
