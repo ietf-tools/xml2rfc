42279681cd37eaa834ba560193107827da3949d5
Paul Hoffman <paul.hoffman@vpnc.org>
2016-01-07 23:27:48 +0000

    More changes from Joe  - Legacy-Id: 2116

--- vocabulary/v3/latest/xml2rfcvoc.mk
+++ vocabulary/v3/latest/xml2rfcvoc.mk
@@ -1,5 +1,5 @@
 xml2rfc.all: \
-	draft-hoffman-xml2rfc-latest.xml xml2rfcv3-annotated.rng
+	draft-iab-xml2rfc-latest.xml xml2rfcv3-annotated.rng
 
 xml2rfcv3.rnc: xml2rfcv3.rng
 	java -jar ../../tools/trang.jar -o lineLength=69 $< $@
@@ -7,19 +7,19 @@ xml2rfcv3.rnc: xml2rfcv3.rng
 #xml2rfcv3.dtd: xml2rfcv3.rng
 #	java -jar ../../tools/trang.jar $< $@
 
-xml2rfcv3-annotated.rng: xml2rfcv3.rng annotate-rng.xslt draft-hoffman-xml2rfc-latest.xml
-	saxon $< annotate-rng.xslt doc=draft-hoffman-xml2rfc-latest.xml > $@
+xml2rfcv3-annotated.rng: xml2rfcv3.rng annotate-rng.xslt draft-iab-xml2rfc-latest.xml
+	saxon $< annotate-rng.xslt doc=draft-iab-xml2rfc-latest.xml > $@
 
 xml2rfcv3-spec.xml: xml2rfcv3.rng rng2xml2rfc.xslt
-	saxon $< rng2xml2rfc.xslt voc=v3 specsrc=draft-hoffman-xml2rfc-latest.xml > $@
+	saxon $< rng2xml2rfc.xslt voc=v3 specsrc=draft-iab-xml2rfc-latest.xml > $@
 
 xml2rfcv3-spec-deprecated.xml: xml2rfcv3.rng rng2xml2rfc.xslt
-	saxon $< rng2xml2rfc.xslt specsrc=draft-hoffman-xml2rfc-latest.xml deprecated=yes > $@
+	saxon $< rng2xml2rfc.xslt specsrc=draft-iab-xml2rfc-latest.xml deprecated=yes > $@
 
 xml2rfcv3.rnc.folded: xml2rfcv3.rnc
 	./fold-rnc.sh $< > $@
 
-draft-hoffman-xml2rfc-latest.xml: xml2rfcv3-spec.xml xml2rfcv3-spec-deprecated.xml xml2rfcv3.rnc.folded differences-from-v2.txt
+draft-iab-xml2rfc-latest.xml: xml2rfcv3-spec.xml xml2rfcv3-spec-deprecated.xml xml2rfcv3.rnc.folded differences-from-v2.txt
 	cp -v $@ $@.bak
 	./refresh-inclusions.sh $@
 
