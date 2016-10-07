e0f9a133740f6641f8980a5f058e112cea01a045
Henrik Levkowetz <henrik@levkowetz.com>
2016-10-07 15:09:15 +0000

    Reverted version for new mkrelease run.  - Legacy-Id: 2262

--- cli/xml2rfc/__init__.py
+++ cli/xml2rfc/__init__.py
@@ -1,7 +1,7 @@
 
 
 # Static values
-__version__  = '2.5.2.dev0'
+__version__  = '2.5.2'
 NAME         = 'xml2rfc'
 VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
 CACHES       = ['/var/cache/xml2rfc', '~/.cache/xml2rfc']  # Ordered by priority
