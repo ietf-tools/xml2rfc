ddc41fb5976ea2af1bcbb4921e350002fe373695
Henrik Levkowetz <henrik@levkowetz.com>
2018-10-11 15:09:59 +0000

    Renamed html.py to legacy_html.py in preparation for work on the v3 html formatter.  - Legacy-Id: 2626

--- cli/xml2rfc/writers/__init__.py
+++ cli/xml2rfc/writers/__init__.py
@@ -3,13 +3,14 @@ from xml2rfc.writers.base import RfcWriterError
 from xml2rfc.writers.base import BaseRfcWriter
 from xml2rfc.writers.raw_txt import RawTextRfcWriter
 from xml2rfc.writers.paginated_txt import PaginatedTextRfcWriter
-from xml2rfc.writers.html import HtmlRfcWriter
+from xml2rfc.writers.legacy_html import HtmlRfcWriter
 from xml2rfc.writers.nroff import NroffRfcWriter
 from xml2rfc.writers.expanded_xml import ExpandedXmlWriter
 from xml2rfc.writers.v2v3 import V2v3XmlWriter
 from xml2rfc.writers.preptool import PrepToolWriter
 from xml2rfc.writers.text import TextWriter
 
+# This defines what 'from xml2rfc.riters import *' actually imports:
 __all__ = ['BaseRfcWriter', 'RawTextRfcWriter', 'PaginatedTextRfcWriter',
            'HtmlRfcWriter', 'NroffRfcWriter', 'ExpandedXmlWriter',
            'RfcWriterError', 'V2v3XmlWriter', 'PrepToolWriter', 'TextWriter', ]
