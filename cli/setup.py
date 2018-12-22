22f2dd0492456a404990be59da36d3605204463f
Henrik Levkowetz <henrik@levkowetz.com>
2018-12-22 15:52:40 +0000

    Set version info to release version 2.16.0 before tagging.  - Legacy-Id: 2885

--- cli/setup.py
+++ cli/setup.py
@@ -81,7 +81,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.15.5',
+    version='2.16.0',
     author='Henrik Levkowetz, Josh Bothun',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
--- cli/xml2rfc/__init__.py
+++ cli/xml2rfc/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals, print_function, division
 
 # Static values
-__version__  = '2.15.5'
+__version__  = '2.16.0'
 NAME         = 'xml2rfc'
 VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
 CACHES       = ['/var/cache/xml2rfc', '~/.cache/xml2rfc']  # Ordered by priority
