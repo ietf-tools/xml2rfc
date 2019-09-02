3712523cb325bb43b1b164e833dd436c1c62a295
Henrik Levkowetz <henrik@levkowetz.com>
2019-09-02 18:22:37 +0000

    Fixed an issue with nested <ul> with emtpy="true".  - Legacy-Id: 3265

--- cli/xml2rfc/writers/text.py
+++ cli/xml2rfc/writers/text.py
@@ -1944,7 +1944,7 @@ class TextWriter(BaseV3Writer):
         tt, __ = self.text_or_block_renderer(e, width, **kwargs)
         if isinstance(tt, list):
             lines = stripl(tt)
-            if lines and lines[0].elem.tag not in ['artwork', 'figure', 'sourcecode', ]:
+            if lines and lines[0].elem.tag not in ['artwork', 'figure', 'sourcecode', 'li', ]:
                 lines[0].text = text + lines[0].text.lstrip()
         else:
             text += tt.lstrip()
