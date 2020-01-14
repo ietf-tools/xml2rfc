441e51c014b9bff69ab1b0c74e81f1066a53b4c0
Henrik Levkowetz <henrik@levkowetz.com>
2020-01-14 18:35:01 +0000

    Set version info to release version 2.38.0 before tagging.  - Legacy-Id: 3559

--- cli/setup.py
+++ cli/setup.py
@@ -81,7 +81,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='2.37.3',
+    version='2.38.0',
     author='Henrik Levkowetz, Josh Bothun',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
