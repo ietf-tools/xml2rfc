2349fc74e9f1068800fdbbe582242ced0e2890b1
Henrik Levkowetz <henrik@levkowetz.com>
2020-09-24 09:51:45 +0000

    Updated the utility script that checks for test and development requirements and font installation.  - Legacy-Id: 3822

--- cli/configtest.py
+++ cli/configtest.py
@@ -8,37 +8,54 @@ import warnings
 warnings.filterwarnings("ignore", message='There are known rendering problems and missing features with cairo < 1.15.4')
 
 errors = 0
+
+sys.stderr.write("Checking installation of test and development packages:\n")
 for (pname, mname) in [
             ('decorator', 'decorator'),
             ('dict2xml', 'dict2xml'),
             ('pycairo', 'cairo'),
             ('PyPDF2', 'PyPDF2'),
-            ('python-fontconfig', 'fontconfig'),
-            ('tox', 'tox'),
-            ('weasyprint', 'weasyprint'),
         ]:
     try:
+        sys.stderr.write("  '%s'...\n" % pname)
         m = importlib.import_module(mname)
-    except ModuleNotFoundError:
+    except ImportError:
         errors += 1
-        sys.stderr.write("  Missing package: %s\n" % (pname, ))
+        sys.stderr.write("  Missing package: '%s'\n" % (pname, ))
 
 if errors:
-    sys.stderr.write("Not all test requirements are fulfilled")
+    sys.stderr.write("Not all test requirements are fulfilled\n")
     sys.exit(errors)
 
-import fontconfig
+try:
+    import fontconfig
+except ImportError:
+    sys.stderr.write("Python-fontconfig not installed, will try to use fontconfig command-line tools\n"
+                     "Install python-fonconfig for better performance.\n")
+    fontconfig = None
+
 for (fname, fcount) in [
             ('Noto Serif', 72),
             ('Roboto Mono', 10)
         ]:
-    available = fontconfig.query(family=fname)
-    acount = len(available)
+    if fontconfig:
+        available = fontconfig.query(family=fname)
+        acount = len(available)
+    else:
+        # try external program
+
+        import subprocess
+        done = subprocess.run(["fc-list", fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if done.returncode == 0:
+            available = done.stdout.decode().splitlines()
+            acount = len(available)
+        else:
+            sys.exit(done.stderr)
     if acount < fcount:
         errors += 1
         sys.stderr.write("  Missing fonts: found %s fonts for family %s, but expected at least %s\n" % (acount, fname, fcount))
 
 if errors:
-    sys.stderr.write("Not all test requirements are fulfilled")
+    sys.stderr.write("Not all test requirements are fulfilled\n")
     sys.exit(errors)
 
