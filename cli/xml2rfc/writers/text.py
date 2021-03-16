0979a66dd0dd2e403bed80f13b7811a689d39e31
Robert Sparks <rjsparks@nostrum.com>
2021-03-16 16:11:44 +0000

    Merged in [3887] and [3889] from jennifer@painless-security.com:\n    Simplify text rendering of super/subscripts. Based on patch submitted by <martin.thomson@gmail.com> and refinement from subsequent list discussion. Fixes #590.  - Legacy-Id: 3904 Note: SVN reference [3887] has been migrated to Git commit 65f2676627e6e79a7857b3dfb2c2bd36fb22c8ec
    Note: SVN reference [3889] has been migrated to Git commit 28d2f44892cf999f48a1a239df168e814325780f

--- cli/xml2rfc/writers/text.py
+++ cli/xml2rfc/writers/text.py
@@ -3581,6 +3581,54 @@ class TextWriter(BaseV3Writer):
         return text
 
 
+    @staticmethod
+    def is_simple_expression(expr):
+        """Can this expression be rendered without adding parentheses?
+
+        Accepts (returns True for) a single alphanumeric string with no whitespace,
+        optionally preceded by a sign character. A non-integer decimal number is
+        accepted as long as it precedes any alphabetic characters. If the expression,
+        excluding an allowed leading sign character, is surrounded by balanced parentheses,
+        True is returned regardless of the contents.
+        """
+        def already_parenthesized(s):
+            """Is the string enclosed in parentheses?
+
+            Only considers parentheses, not other brackets. Good enough to avoid
+            pointlessly doubling the parentheses, not to decide that the expression
+            makes mathematical sense.
+            """
+            if not (len(s) >= 2 and s[0] == '(' and s[-1] == ')'):
+                return False
+            count = 0
+            for c in s[1:-1]:
+                count += 1 if c == '(' else -1 if c == ')' else 0
+                if count < 0:
+                    return False
+            return count == 0
+
+        # Leading sign is allowed, so ignore it for further tests. Accept unicode
+        # sign chars '\u2212' (negative sign), '\u00b1' (plus/minus), '\u2213' (minus/plus),
+        # '\ufe63' (small minus),'\uff0b' (full-width plus), '\uff0d' (full-width minus)
+        if len(expr) > 0 and expr[0] in '+-\u2212\u00b1\u2213\ufe63\uff0b\uff0d':
+            expr = expr[1:]
+
+        # Empty or all-whitespace after removing sign must have parentheses for clarity
+        if len(expr) == 0:
+            return False
+
+        # Avoid (( )) if the entire expression is already in balanced parentheses
+        if already_parenthesized(expr):
+            return True
+
+        # Underscore is a `\w` character, so explicitly reject it
+        if '_' in expr:
+            return False
+
+        # Regex accepts number (with or without decimal point) followed by mixed word characters.
+        # Assumes already removed sign and checked for empty string.
+        return re.match(r'^(?:\d+(?:\.\d+)?)?\w*$', expr) is not None
+
     # 2.51.  <sub>
     # 
     #    Causes the text to be displayed as subscript, approximately half a
@@ -3588,7 +3636,11 @@ class TextWriter(BaseV3Writer):
     #    with other character formatting elements, and the formatting will be
     #    additive.
     def render_sub(self, e, width, **kwargs):
-        text = '_(%s)' % self.inner_text_renderer(e)
+        inner = self.inner_text_renderer(e)
+        if self.is_simple_expression(inner):
+            text = '_%s' % inner
+        else:
+            text = '_(%s)' % inner
         text += e.tail or ''
         return text
 
@@ -3600,7 +3652,11 @@ class TextWriter(BaseV3Writer):
     #    with other character formatting elements, and the formatting will be
     #    additive.
     def render_sup(self, e, width, **kwargs):
-        text = '^(%s)' % self.inner_text_renderer(e)
+        inner = self.inner_text_renderer(e)
+        if self.is_simple_expression(inner):
+            text = '^%s' % inner
+        else:
+            text = '^(%s)' % inner
         text += e.tail or ''
         return text
 
