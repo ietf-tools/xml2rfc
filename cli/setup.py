59aa57e79ac7b1ddb02225d6572f04e33d1c41e9
Robert Sparks <rjsparks@nostrum.com>
2021-06-25 19:02:18 +0000

    Set version info to release version 3.9.1 before tagging.  - Legacy-Id: 3996

--- cli/setup.py
+++ cli/setup.py
@@ -81,7 +81,7 @@ Changelog
 setup(
     # Package metadata
     name='xml2rfc',
-    version='3.9.0',
+    version='3.9.1',
     author='Henrik Levkowetz',
     author_email='tools-discuss@ietf.org',
     maintainer = "Henrik Levkowetz",
