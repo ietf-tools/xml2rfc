e5e54d9f1a8e64671342fe623b86b531dc8eb0b7
NGPixel <github@ngpixel.com>
2022-02-09 15:51:01 -0500

    chore: move /package to root

--- xml2rfc/data/xml2rfc.js
+++
@@ -1,7 +0,0 @@
-const toc = document.getElementById("toc");
-toc.querySelector("h2").addEventListener("click", e => {
-  toc.classList.toggle("active");
-});
-toc.querySelector("nav").addEventListener("click", e => {
-  toc.classList.remove("active");
-});
