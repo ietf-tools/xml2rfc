35da1aa3d617f5d658fae2d081ddceffebb62f38
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-06 13:06:39 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker --version
-
-echo "Build the image..."
-docker build --pull -t xml2rfc .
-echo "Starting xml2rfc docker image..."
-docker run -it -v ${PWD}:/root/xml2rfc xml2rfc
