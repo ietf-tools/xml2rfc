770348a434fe32c2ae2770d30316d97f9cf9d87d
Kesara Rathnayake <kesara@fq.nz>
2022-08-03 11:32:35 +1200

    build: Use base.Dockerfile for dev.Dockerfile [skip ci]

--- run.sh
+++ run.sh
@@ -2,7 +2,18 @@
 
 docker --version
 
-echo "Build the image..."
-docker build --pull -t xml2rfc .
+if [[ "$(docker images -q xml2rfc-base:latest 2> /dev/null)" == "" ]]; then
+    echo "Build the image xml2rfc-base:latest"
+    docker build -f docker/base.Dockerfile --pull -t xml2rfc-base:latest .
+else
+    echo "xml2rfc-base:latest already exists."
+    echo "To update xml2rfc-base, run:"
+    echo "      docker rmi xml2rfc-base:latest"
+    echo "and rerun this script."
+fi
+
+echo "Build the image xml2rfc-dev"
+docker build -f docker/dev.Dockerfile -t xml2rfc-dev .
+
 echo "Starting xml2rfc docker image..."
-docker run -it -v ${PWD}:/root/xml2rfc xml2rfc
+docker run -it -v ${PWD}:/root/xml2rfc xml2rfc-dev
