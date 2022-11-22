b451deda56d8d38276d6554043adb8c3325dcdd2
Kesara Rathnayake <kesara@fq.nz>
2022-11-23 09:07:39 +1300

    build: Add support for Python 3.11 (#942)

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     license="BSD-3-Clause",
 
