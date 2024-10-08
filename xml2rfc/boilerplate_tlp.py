56da31184dc50468d3cadda40984f342642a3396
Kesara Rathnayake <kesara@fq.nz>
2024-10-09 11:31:52 +1300

    feat: Update boilerplate to TLP 6.0
    This changes copyright from IETF Trust to IETF Intellectual Property Management
    Corporation (IPMC).
    
    This introduces boilerplate version 6.0.
    
    This introduces the following new `ipr` values for v3 specification:
        * `tpl202412`
        * `noModificationTpl202412`
        * `noDerivativesTpl202412`
        * `pre5378Tpl202412`
    
    This sets `tpl202412` as the default value for `ipr` attribute.
    
    Fixes #1171

--- xml2rfc/boilerplate_tlp.py
+++ xml2rfc/boilerplate_tlp.py
@@ -255,4 +255,75 @@ boilerplate_tlp = {
             </t>""",
         ],
     },
+    "6.0": {
+        # TODO: Update the source URL
+        "draft": [
+            """<t>
+            This Internet-Draft is submitted in full conformance with the
+            provisions of BCP 78 and BCP 79.
+            </t>""",
+        ],
+        "IETF": [
+            """<t>
+            Copyright (c) {year} IETF Intellectual Property Management
+            Corporation (IPMC) and the persons identified as the
+            document authors. All rights reserved.
+            </t>""",
+            """<t>
+            This document is subject to BCP 78 and the IETF IPMC's Legal
+            Provisions Relating to IETF Documents
+            (<eref target="{scheme}://trustee.ietf.org/license-info"/>) in effect on the date of
+            publication of this document. Please review these documents
+            carefully, as they describe your rights and restrictions with
+            respect to this document. Code Components extracted from this
+            document must include Revised BSD License text as described in
+            Section 4.e of the IPMC Legal Provisions and are provided without
+            warranty as described in the Revised BSD License.
+            </t>""",
+        ],
+        "alt": [
+            """<t>
+            Copyright (c) {year} IETF Intellectual Property Management
+            Corporation (IPMC) and the persons identified as the
+            document authors. All rights reserved.
+            </t>""",
+            """<t>
+            This document is subject to BCP 78 and the IETF IPMC's Legal
+            Provisions Relating to IETF Documents
+            (<eref target="{scheme}://trustee.ietf.org/license-info"/>) in effect on the date of
+            publication of this document. Please review these documents
+            carefully, as they describe your rights and restrictions with
+            respect to this document.
+            </t>""",
+        ],
+        "noModification": [
+            """<t>
+            This document may not be modified, and derivative works of it may
+            not be created, except to format it for publication as an RFC or
+            to translate it into languages other than English.
+            </t>""",
+        ],
+        "noDerivatives": [
+            """<t>
+            This document may not be modified, and derivative works of it may
+            not be created, and it may not be published except as an
+            Internet-Draft.
+            </t>""",
+        ],
+        "pre5378": [
+            """<t>
+            This document may contain material from IETF Documents or IETF
+            Contributions published or made publicly available before November
+            10, 2008. The person(s) controlling the copyright in some of this
+            material may not have granted the IETF IPMC the right to allow
+            modifications of such material outside the IETF Standards Process.
+            Without obtaining an adequate license from the person(s)
+            controlling the copyright in such materials, this document may not
+            be modified outside the IETF Standards Process, and derivative
+            works of it may not be created outside the IETF Standards Process,
+            except to format it for publication as an RFC or to translate it
+            into languages other than English.
+            </t>""",
+        ],
+    },
 }
