40efa8ff755e24540479af391e266845abac84e9
Kesara Rathnayake <kesara@fq.nz>
2024-10-12 00:17:33 +1300

    fix: Change IPMC to IPM
    Drop corporation from the name.

--- xml2rfc/boilerplate_tlp.py
+++ xml2rfc/boilerplate_tlp.py
@@ -265,30 +265,28 @@ boilerplate_tlp = {
         ],
         "IETF": [
             """<t>
-            Copyright (c) {year} IETF Intellectual Property Management
-            Corporation (IPMC) and the persons identified as the
-            document authors. All rights reserved.
+            Copyright (c) {year} IETF Intellectual Property Management (IPM)
+            and the persons identified as the document authors. All rights reserved.
             </t>""",
             """<t>
-            This document is subject to BCP 78 and the IETF IPMC's Legal
+            This document is subject to BCP 78 and the IETF IPM's Legal
             Provisions Relating to IETF Documents
             (<eref target="{scheme}://trustee.ietf.org/license-info"/>) in effect on the date of
             publication of this document. Please review these documents
             carefully, as they describe your rights and restrictions with
             respect to this document. Code Components extracted from this
             document must include Revised BSD License text as described in
-            Section 4.e of the IPMC Legal Provisions and are provided without
+            Section 4.e of the IPM Legal Provisions and are provided without
             warranty as described in the Revised BSD License.
             </t>""",
         ],
         "alt": [
             """<t>
-            Copyright (c) {year} IETF Intellectual Property Management
-            Corporation (IPMC) and the persons identified as the
-            document authors. All rights reserved.
+            Copyright (c) {year} IETF Intellectual Property Management (IPM)
+            and the persons identified as the document authors. All rights reserved.
             </t>""",
             """<t>
-            This document is subject to BCP 78 and the IETF IPMC's Legal
+            This document is subject to BCP 78 and the IETF IPM's Legal
             Provisions Relating to IETF Documents
             (<eref target="{scheme}://trustee.ietf.org/license-info"/>) in effect on the date of
             publication of this document. Please review these documents
@@ -315,7 +313,7 @@ boilerplate_tlp = {
             This document may contain material from IETF Documents or IETF
             Contributions published or made publicly available before November
             10, 2008. The person(s) controlling the copyright in some of this
-            material may not have granted the IETF IPMC the right to allow
+            material may not have granted the IETF IPM the right to allow
             modifications of such material outside the IETF Standards Process.
             Without obtaining an adequate license from the person(s)
             controlling the copyright in such materials, this document may not
