2cbce15d97e543c5f53ea50bd6d0af8a7ecb31c2
Kesara Rathnayake <kesara@fq.nz>
2024-10-23 10:30:51 +1300

    fix: Update 6.0 boilerplate to say "IETF IPM Technical Legal Provisions"

--- xml2rfc/boilerplate_tlp.py
+++ xml2rfc/boilerplate_tlp.py
@@ -269,14 +269,14 @@ boilerplate_tlp = {
             and the persons identified as the document authors. All rights reserved.
             </t>""",
             """<t>
-            This document is subject to BCP 78 and the  IETF IPM Technical
+            This document is subject to BCP 78 and the  IETF IPM Technical Legal
             Provisions Relating to IETF Documents
             (<eref target="{scheme}://trustee.ietf.org/license-info"/>) in effect on the date of
             publication of this document. Please review these documents
             carefully, as they describe your rights and restrictions with
             respect to this document. Code Components extracted from this
             document must include Revised BSD License text as described in
-            Section 4.e of the IPM Legal Provisions and are provided without
+            Section 4.e of the IETF IPM Technical Legal Provisions and are provided without
             warranty as described in the Revised BSD License.
             </t>""",
         ],
@@ -286,7 +286,7 @@ boilerplate_tlp = {
             and the persons identified as the document authors. All rights reserved.
             </t>""",
             """<t>
-            This document is subject to BCP 78 and the  IETF IPM Technical
+            This document is subject to BCP 78 and the  IETF IPM Technical Legal
             Provisions Relating to IETF Documents
             (<eref target="{scheme}://trustee.ietf.org/license-info"/>) in effect on the date of
             publication of this document. Please review these documents
